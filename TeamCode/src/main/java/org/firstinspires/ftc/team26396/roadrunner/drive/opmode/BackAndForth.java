74b46708ce52cee609fa03981b1ede265d2f6c53
Vybhav26 <117322739+Vybhav26@users.noreply.github.com>
2024-10-16 10:23:08 -0500

    Some Auto Work

--- TeamCode/src/main/java/org/firstinspires/ftc/team26396/roadrunner/drive/opmode/BackAndForth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team26396/roadrunner/drive/opmode/BackAndForth.java
@@ -25,7 +25,7 @@ import org.firstinspires.ftc.team26396.roadrunner.drive.SampleMecanumDrive;
  * is recommended that you use the FollowerPIDTuner opmode for further fine tuning.
  */
 @Config
-@Autonomous(group = "drive")
+@Autonomous(group = "BackAndForthDrive")
 public class BackAndForth extends LinearOpMode {
 
     public static double DISTANCE = 50;
