fa5659ba8f0cd08204f55db9e2aa3759bd2fd796
26niljia <26niljia@elmbrookstudents.org>
2024-12-31 14:12:11 -0600

    Pinpoint's back

--- TeamCode/src/main/java/org/firstinspires/ftc/team26396/roadrunner/teamcode/tuning/ManualFeedbackTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team26396/roadrunner/teamcode/tuning/ManualFeedbackTuner.java
@@ -4,10 +4,10 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.TwoDeadWheelLocalizer;
+import org.firstinspires.ftc.team26396.roadrunner.teamcode.MecanumDrive;
+import org.firstinspires.ftc.team26396.roadrunner.teamcode.TankDrive;
+import org.firstinspires.ftc.team26396.roadrunner.teamcode.ThreeDeadWheelLocalizer;
+import org.firstinspires.ftc.team26396.roadrunner.teamcode.TwoDeadWheelLocalizer;
 
 public final class ManualFeedbackTuner extends LinearOpMode {
     public static double DISTANCE = 64;
