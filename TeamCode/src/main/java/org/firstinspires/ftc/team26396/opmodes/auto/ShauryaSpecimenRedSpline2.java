1f7874d3e7be0e8a9ae427d12f4b1437fcc505ee
shauryathakur <shauryat215@gmail.com>
2025-01-10 19:50:28 -0600

    RoadRunner w Motors on RedSpline2

--- TeamCode/src/main/java/org/firstinspires/ftc/team26396/opmodes/auto/ShauryaSpecimenRedSpline2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team26396/opmodes/auto/ShauryaSpecimenRedSpline2.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.team26396.opmodes.auto;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import androidx.annotation.NonNull;
 
 // RR-specific imports
@@ -11,6 +15,13 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -27,6 +38,46 @@ public class ShauryaSpecimenRedSpline2 extends LinearOpMode {
         // Initialize the MecanumDrive with the hardware map
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
+        DcMotorEx linearSlideMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "armMotor");
+
+        DcMotor armMotor = hardwareMap.dcMotor.get("liftMotor");
+        CRServo clawServo = hardwareMap.get(CRServo.class, "claw");
+        Servo wristServo = hardwareMap.get(Servo.class, "wrist");
+        DcMotor HangMotor1 = hardwareMap.dcMotor.get("HM1");
+        DcMotor HangMotor2 = hardwareMap.dcMotor.get("HM2");
+
+
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        //backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        if (isStopRequested()) return;
+
+        // Initialize and configure IMU
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters imuParameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+        imu.initialize(imuParameters);
+
+        // Set zero power behavior
+        linearSlideMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        linearSlideMotor.setPositionPIDFCoefficients(10.0);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        HangMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        HangMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // Wait for the start signal
         waitForStart();
 
