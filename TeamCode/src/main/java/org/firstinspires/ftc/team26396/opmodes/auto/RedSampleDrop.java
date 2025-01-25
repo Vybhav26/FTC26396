0893b183512b879979924292dead01908a52f7b5
shauryathakur <shauryat215@gmail.com>
2025-01-24 20:23:53 -0600

    Red Sample Drop Autopath

--- TeamCode/src/main/java/org/firstinspires/ftc/team26396/opmodes/auto/RedSampleDrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team26396/opmodes/auto/RedSampleDrop.java
@@ -16,22 +16,24 @@ public class RedSampleDrop extends LinearOpMode {
     public void runOpMode() {
         // I'm assuming you're at 0,0
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(20, -60, 0));
+        // Define arm positions using the constants from the Arm class
 
         waitForStart();
 
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0, -60, 0))
-                        //.splineTo(new Vector2d(0, -34), Math.toRadians(0))
+                        .waitSeconds(1)
                         .turnTo(Math.toRadians(90))
                         .lineToY(-36)
                         .waitSeconds(1)
                         .turnTo(Math.toRadians(0))
+                        .waitSeconds(1)
                         .lineToX(38)
                         .turnTo(Math.toRadians(90))
                         .lineToY(-10)
                         .waitSeconds(1)
                         .strafeTo(new Vector2d(44, -10))
-                        .turnTo(Math.toRadians(-90))
+                        .waitSeconds(1)
                         .lineToY(-58)
                         .lineToY(-10)
                         .strafeTo(new Vector2d(54, -10))
